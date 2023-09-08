35d0635282e40fe054c30d2d9800f7abfd462dba
Mura <este.cornejo@duocuc.cl>
2023-09-08 20:13:08 -0300

    AVANZANDO EL QR

--- src/app/main-menu/main-menu.page.ts
+++ src/app/main-menu/main-menu.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-main-menu',
@@ -8,10 +10,19 @@ import { Router } from '@angular/router';
 })
 export class MainMenuPage implements OnInit {
 
-  constructor(private router: Router) { }
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor(private router: Router,
+              private alertController: AlertController) { }
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
   }
+
+  
 
   GoToPerfil() {
     this.router.navigate(['/perfil']);
@@ -21,5 +32,29 @@ export class MainMenuPage implements OnInit {
   }
   GoToAyuda() {
     this.router.navigate(['/ayuda']);
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
